565f0a0750787cdb648626649b8926e7ecf7d36e
Tim Preston <tim@tp.id.au>
2013-03-25 07:12:13 +0800

    Middleman 3.0.13 and Foundation 4.0.9

---
+++ source/javascripts/app.js
@@ -0,0 +1,27 @@
+// Require either Zepto
+//= require vendor/zepto
+
+// or jQuery
+// require vendor/jquery
+
+
+// All Foundation javascript libraries
+//= require foundation
+
+// or pick and choose
+// require foundation/foundation
+// require foundation/foundation.alerts
+// require foundation/foundation.clearing
+// require foundation/foundation.cookie
+// require foundation/foundation.dropdown
+// require foundation/foundation.forms
+// require foundation/foundation.joyride
+// require foundation/foundation.magellan
+// require foundation/foundation.orbit
+// require foundation/foundation.placeholder
+// require foundation/foundation.reveal
+// require foundation/foundation.section
+// require foundation/foundation.tooltips
+// require foundation/foundation.topbar
+
+ $(document).foundation();
